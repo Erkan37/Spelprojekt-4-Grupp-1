d7088b3d1e46d4ae3e998fe5f5d24175e354f842
Linnea <linnea.almlof@studerande.thegameassembly.se>
2021-03-12 17:25:17 +0100

    Parsing platforms (scale is off though)

--- Source/Game/TiledMap.h
+++ Source/Game/TiledMap.h
@@ -8,7 +8,6 @@ namespace tson
 }
 
 class Scene; 
-//class PlatformFactory;
 
 class TiledMap
 {
@@ -16,7 +15,7 @@ public:
 	bool Load(const std::string& aPath, Scene*);
 
 private:
-	void ParseBonefires(tson::Layer*, Scene*);
+	void ParseBonfires(tson::Layer*, Scene*);
 	void ParseDoors(tson::Layer*, Scene*);
 	void ParseEnemies(tson::Layer*, Scene*);
 	void ParseLedges(tson::Layer*, Scene*);
@@ -24,6 +23,7 @@ private:
 	void ParsePlatforms(tson::Layer*, Scene*);
 
 	v2f GetScreenPosition(v2f aTiledPos);
+	v2f GetObjSize(v2f aTiledSize);
 	v2f myNumberOfTilesOnScreen;
 
 	std::unique_ptr<PlatformFactory> myPlatformFactory;
