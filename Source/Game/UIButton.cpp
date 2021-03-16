73821ac066bfac898ab4c2e3fff5ddb502096520
Daniel <71003000+Dodjoj@users.noreply.github.com>
2021-03-16 16:13:35 +0100

    Update UIButton.cpp

--- Source/Game/UIButton.cpp
+++ Source/Game/UIButton.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "UIButton.h"
 #include "GameObject.h"
+#include "SpriteComponent.h"
 
 
 UIButton::UIButton()
@@ -14,7 +15,8 @@ UIButton::~UIButton()
 void UIButton::Init(Scene* aLevelScene)
 {
 	GameObject button = GameObject(aLevelScene);
-
+	SpriteComponent* sprite = button.AddComponent<SpriteComponent>();
+	sprite->SetSpritePath("Sprites/UI/UI_PauseMenu_Text_Continue_Unmarked_64x16px.dds");
 
 
 }
