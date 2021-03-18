d58437b23e57f2627c53309f12dc5388b6e92929
Daniel <71003000+Dodjoj@users.noreply.github.com>
2021-03-18 15:25:16 +0100

    Update MainMenuScene.cpp

--- Source/Game/MainMenuScene.cpp
+++ Source/Game/MainMenuScene.cpp
@@ -2,24 +2,32 @@
 #include "MainMenuScene.h"
 #include "UIObject.h"
 
+
 MainMenuScene::MainMenuScene()
 {
 }
 
 void MainMenuScene::Load()
 {
-	myBackground = std::make_unique<UIObject>();
-	myBackground->Init();
+	myBackground = std::make_unique<UIObject>(this);
+	v2f backgroundPos = { 5.f, 5.f };
+
+	myBackground->Init("Sprites/UI/pauseMenu/UI_PauseMenu_Bakground_304x164px.dds", { 520.f, 265.f }, backgroundPos, 599);
+
+	Scene::Load();
 }
 
 void MainMenuScene::Activate()
 {
+	Scene::Activate();
 }
 
 void MainMenuScene::Deactivate()
 {
+	Scene::Deactivate();
 }
 
 void MainMenuScene::Update(const float& aDeltaTime)
 {
+	myBackground->UpdateUIObjects(aDeltaTime);
 }
