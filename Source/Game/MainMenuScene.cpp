01d8a1d84250c49ea6a4d531658b46f705f558a5
Daniel <71003000+Dodjoj@users.noreply.github.com>
2021-03-18 14:28:04 +0100

    Addin main menu scene

--- Source/Game/MainMenuScene.cpp
+++ Source/Game/MainMenuScene.cpp
@@ -1,3 +1,22 @@
 #include "stdafx.h"
 #include "MainMenuScene.h"
 
+MainMenuScene::MainMenuScene()
+{
+}
+
+void MainMenuScene::Load()
+{
+}
+
+void MainMenuScene::Activate()
+{
+}
+
+void MainMenuScene::Deactivate()
+{
+}
+
+void MainMenuScene::Update(const float& aDeltaTime)
+{
+}
--- Source/Game/MainMenuScene.h
+++ Source/Game/MainMenuScene.h
@@ -3,6 +3,21 @@
 
 class MainMenuScene : public Scene
 {
+public:
+
+    MainMenuScene();
+
+    void Load() override;
+
+    void Activate() override;
+    void Deactivate() override;
+
+    void Update(const float& aDeltaTime) override;
+
+
+private:
+
+
 
 };
 
