2309e13dd5c0d11555abd281c7daa37354027798
AxelMelkersson <axel.melkersson@studerande.thegameassembly.se>
2021-03-19 16:33:43 +0100

    TileSetLayer

---
+++ Source/Game/TileSetLayerProperties.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "TileSetLayerProperties.hpp"
+
+#include "../External/Headers/rapidjson/document.h"
+#include "../External/Headers/rapidjson/istreamwrapper.h"
+#include <fstream>
+
+TileSetLayerProperties::TileSetLayerProperties()
+{
+	std::ifstream tilesetFile = std::ifstream("Levels/PassOnTileset.json");
+	rapidjson::IStreamWrapper tilesetFileStream(tilesetFile);
+
+	rapidjson::Document tileSet;
+	tileSet.ParseStream(tilesetFileStream);
+
+	mySpritePath = tileSet["image"].GetString();
+
+	myQuadLengthX = tileSet["columns"].GetInt();
+	myQuadLengthY = tileSet["tilecount"].GetInt() / myQuadLengthX;
+	mySpriteSizeX = tileSet["tilewidth"].GetFloat();
+	mySpriteSizeY = tileSet["tileheight"].GetFloat();
+	myImageSizeX = tileSet["imagewidth"].GetInt();
+	myImageSizeY = tileSet["imageheight"].GetInt();
+
+	float bufferX = (mySpriteSizeX * myQuadLengthX) / myImageSizeX;
+	float bufferY = (mySpriteSizeY * myQuadLengthY) / myImageSizeY;
+
+	myRectQuadX = bufferX / myQuadLengthX;
+	myRectQuadY = bufferY / myQuadLengthY;
+}
