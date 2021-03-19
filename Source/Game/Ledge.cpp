7981b1df123b9b6e34d380c64f6ec3c52c9859f4
AxelMelkersson <axel.melkersson@studerande.thegameassembly.se>
2021-03-19 09:51:51 +0100

    Builds

--- Source/Game/Ledge.cpp
+++ Source/Game/Ledge.cpp
@@ -28,10 +28,6 @@ void Ledge::Init(const v2f& aPosition, const v2f& aSize)
 	SetPosition(aPosition + v2f(0.0f, 13.0f));
 	SetPivot({ 0.5f, 0.0f });
 
-	//SpriteComponent* gsprite = AddComponent<SpriteComponent>();
-	//gsprite->SetSpritePath("Sprites/debugLedge.dds");
-	//gsprite->SetSize(aSize);
-
 	PhysicsComponent* gphys = AddComponent<PhysicsComponent>();
 	gphys->SetCanCollide(false);
 	gphys->SetIsStatic(true);
