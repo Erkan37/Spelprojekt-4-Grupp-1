2abfd95eefdd4fd0f280f637d6ff3fc44a021c6e
AxelMelkersson <axel.melkersson@studerande.thegameassembly.se>
2021-03-19 14:59:59 +0100

    Removed old factories

--- Source/Game/BashableObjectFactory.hpp
+++ Source/Game/BashableObjectFactory.hpp
@@ -7,8 +7,6 @@ class Scene;
 class BashableObjectFactory
 {
 public:
-	void ReadBashableObjects(Scene* aLevelScene, const std::string& aFilePath);
-
 	BashableObject* CreateBashableObject(Scene* aLevelScene, const v2f& aPosition, const float& aRadius, const float& aSpeed, const std::vector<v2f>& aWaypoints);
 
 private:
