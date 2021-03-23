d6df3c90620f9f2c077f84577eeca570b9952640
AxelMelkersson <axel.melkersson@studerande.thegameassembly.se>
2021-03-23 18:18:22 +0100

    Builds

--- Source/Game/Player.cpp
+++ Source/Game/Player.cpp
@@ -582,6 +582,9 @@ void Player::BounceOnDestructibleWall()
 {
 	v2f dashVelocity = myBashAbility->GetVelocity();
 	dashVelocity.x *= -0.8f;
+
+	myBashAbility->InvertDashDirectionX();
+	myBashAbility->SetVelocity(dashVelocity);
 }
 
 void Player::Kill()
