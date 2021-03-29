4a04a9f53e9e8cd1de067b5017ced35a60864130
Benediktas-Vilkelis <71002886+Benediktas-Vilkelis@users.noreply.github.com>
2021-03-29 15:26:33 +0200

    Added the vol percentage thing

--- Source/Game/AudioClip.h
+++ Source/Game/AudioClip.h
@@ -11,7 +11,7 @@ class AudioClip
 {
 public:
 	AudioClip() = default;
-	AudioClip(const char* anAudioPath, const bool aIsLooping, const float& aVolume, const float& aMinVol, const float& aMaxVol, AudioLayer aLayer);
+	AudioClip(const char* anAudioPath, const bool aIsLooping, const float& aVolume, const float& aMaxVol, AudioLayer aLayer);
 	~AudioClip();
 
 	void SetVolume(const float& aVolChange);
@@ -19,6 +19,7 @@ public:
 	void SetPosition(const VECTOR2F aPosition);
 
 	void Play();
+	void PlayIfAvailable();
 	void Stop();
 
 	void Mute();
@@ -30,10 +31,13 @@ public:
 	AudioLayer GetLayer();
 
 private:
+	float GetVolPercentage(const float& aVolume);
+
 	float myVolume = 0.0f;
-	const float myMinVolume;
 	const float myMaxVolume;
+	const float myVolProcent;
 	bool myCanPlay = true;
+	bool myIsPlaying = false;
 	AudioLayer myLayer;
 	Tga2D::CAudio* myAudio = nullptr;
 };
