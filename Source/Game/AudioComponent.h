47618e3a0a2c6d0212db259f74455160bd1c9cda
Benediktas-Vilkelis <71002886+Benediktas-Vilkelis@users.noreply.github.com>
2021-03-30 17:52:02 +0200

    Fixed Audio Components

--- Source/Game/AudioComponent.h
+++ Source/Game/AudioComponent.h
@@ -9,23 +9,28 @@ class AudioComponent : public Component
 {
 public:
 	AudioComponent();
+	~AudioComponent();
 	
-	void AddAudio(AudioList aSound, Scene* aScene);
+	void AddAudio(AudioList aSound);
 	void PlayAudio();
 	void StopAudio();
 	void SetRadius(const float& aRadius);
 	void LockAudio(AudioList aSound);
 	void UnLockAudio(AudioList aSound);
 
+
 	void Update(Transform& aTransform, GameObject& aGameObject) override;
 
+	float myVolume = 0;
+	AudioList myAudio;
 private:
 	std::vector<AudioList> myAudioList;
 	float myRadius = 150.0f;
 	float myMinVolume = 0.1f;
 	float myMaxVolume = 0.4f;
 	float myAudioSpectrum = 0;
+	float myDistanceToPlayer = 0;
+	bool myHasBeenAdded = false;
 	Scene* myScene;
-	AudioList myAudio;
 };
 
