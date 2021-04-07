f7603bcfbea61e943d8d09b8282686b7dee3cfc0
Daniel <71003000+Dodjoj@users.noreply.github.com>
2021-04-07 10:14:06 +0200

    Removed Spawneffect on LMB

--- Source/Game/LevelScene.cpp
+++ Source/Game/LevelScene.cpp
@@ -98,14 +98,6 @@ void LevelScene::Deactivate()
 
 void LevelScene::Update(const float& aDeltaTime)
 {
-
-	if (CGameWorld::GetInstance()->Input()->GetInput()->GetKeyJustDown(Keys::LeftMouseButton))
-	{
-		v2f position = GetPlayer()->GetPosition();
-
-		myEffectFactory->SpawnEffect(position, eParticleEffects::BulletEffectTrail);
-	}
-
 	const float zoomX = CGameWorld::GetInstance()->Game()->GetZoomX();
 	const float zoomY = CGameWorld::GetInstance()->Game()->GetZoomY();
 
