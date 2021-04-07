99438787cc0c0ed9ace0ae8613cf0157e6fbfd0a
T.B <71003024+Theo-Bjorkhem@users.noreply.github.com>
2021-04-07 15:35:53 +0200

    HighScores Can Now Be Saved

--- Source/Game/SpeedrunManager.cpp
+++ Source/Game/SpeedrunManager.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "SpeedrunManager.h"
+#include "DataManager.h"
 
 void SpeedrunManager::SetIsSpeedrun(const bool aIsSpeedrun)
 {
@@ -9,7 +10,6 @@ void SpeedrunManager::SetIsSpeedrun(const bool aIsSpeedrun)
 		myCurrentScore = 0;
 	}
 }
-
 bool SpeedrunManager::GetIsSpeedrun()
 {
 	return myIsSpeedrun;
@@ -19,12 +19,10 @@ float SpeedrunManager::GetScore()
 {
 	return myCurrentScore;
 }
-
 void SpeedrunManager::SetScore(const float aScore)
 {
 	myCurrentScore = aScore;
 }
-
 void SpeedrunManager::AddToScore(const float aScore)
 {
 	myCurrentScore += aScore;
@@ -35,4 +33,8 @@ void SpeedrunManager::ReportScoreToHighscores()
 	//Check if score is a highscore
 	//If it is, add it to list (show to player)
 	//reset current score
+
+	// Call
+	// DataManager::GetInstance().SaveHighScores(myHighScores);
+	// When you have a new Highscore to change.
 }
